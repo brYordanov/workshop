a4c1ffdc88682987fd1a62aee71aab8392373c81
BranimirYo <branimiryordanov75@gmail.com>
2026-02-22 12:35:34 +0200

    state debounce

--- react-playground/src/useDebounce.ts
+++ react-playground/src/useDebounce.ts
@@ -1,19 +1,37 @@
-import { useCallback, useEffect, useRef } from "react"
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useDebounce = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
-    const timerRef = useRef(0)
+export const useDebounce = <T extends (...args: any[]) => any>(
+  fn: T,
+  delay: number
+) => {
+  const timerRef = useRef(0);
 
-    const debouncedFn = useCallback((...args: Parameters<T>) => {
-        clearTimeout(timerRef.current)
-        timerRef.current = setTimeout(() => {
-            fn(...args)
-        }, delay)
-    }, [fn, delay])  
+  const debouncedFn = useCallback(
+    (...args: Parameters<T>) => {
+      clearTimeout(timerRef.current);
+      timerRef.current = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    },
+    [fn, delay]
+  );
 
-    useEffect(()=> {
-        return () => clearTimeout(timerRef.current)
-    }, [])
+  useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
 
-    return debouncedFn
-}
+  return debouncedFn;
+};
+
+export const useDebouncedState = <T>(value: T, delay: number = 300): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
