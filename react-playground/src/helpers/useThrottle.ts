c232000bacdb3a534c4c7997989c45c95651c244
BranimirYo <branimiryordanov75@gmail.com>
2026-02-27 14:35:54 +0200

    wip

--- react-playground/src/helpers/useThrottle.ts
+++ react-playground/src/helpers/useThrottle.ts
@@ -1,14 +1,20 @@
 import { useCallback, useRef } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useThrottle = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
-    const lastRef = useRef(0)
+export const useThrottle = <T extends (...args: any[]) => any>(
+  fn: T,
+  delay: number
+) => {
+  const lastRef = useRef(0);
 
-    return useCallback((...args: Parameters<T>) => {
-        const now = Date.now()
-        if(now - lastRef.current >= delay) {
-            lastRef.current = now
-            fn(...args)
-        } 
-    }, [fn, delay])
-}
+  return useCallback(
+    (...args: Parameters<T>) => {
+      const now = Date.now();
+      if (now - lastRef.current >= delay) {
+        lastRef.current = now;
+        fn(...args);
+      }
+    },
+    [fn, delay]
+  );
+};
